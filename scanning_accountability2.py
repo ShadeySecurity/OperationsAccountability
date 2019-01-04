097994bbcf21c1a87904e76fde8b5182af4bc38a
shadeyshades <abjustinbowen@yahoo.com>
2019-01-04 12:13:18 -0800

    Firewall done.

--- scanning_accountability2.py
+++ scanning_accountability2.py
@@ -21,18 +21,30 @@ class FireWallInit:
 
     def __init__(self):
         # Key is network, value is exceptions
-        self.peer_nets= {"127.0.0.1":["127.0.0.3"]}
-        self.target_nets= {"127.0.0.1":["127.0.0.3"]}
+        self.peer_nets= {"127.0.0.1":{"in":["127.0.0.3"], "out":["127.0.0.4"]}}
+        self.target_nets= {"127.0.0.1":{"in":["127.0.0.3"], "out":["127.0.0.4"]}}
         # single list elements
         self.notouch_nets= []
         self.knownbad_nets= []
-        self.os = "unknown"
+        self.os = {"Family":"unknown","Version":"unknown"}
         # Configuration TODO: Move to config file read in
         self.options = { "policy": { "INPUT":"DROP", "OUTPUT":"DROP", "FORWARD":"DROP"} }
-        self.support_systems = {"ntp": "127.0.0.1", "dns": "1.1.1.1", "share":"127.0.0.1", "dhcp":"127.0.0.1"}
+        self.support_systems = {"ntp": ["127.0.0.1"], "dns": ["1.1.1.1"], "share":["127.0.0.1"], "dhcp":["127.0.0.1"]}
 
     def DetectOs(self):
-        test = 1
+        import platform
+        self.os["Family"] = platform.system()
+        version  = platform.version()
+        if "ubuntu" in version:
+            version = "ubuntu"
+        elif "debian" in version:
+            version = "debian"
+        elif "SMP" in "rhel":
+            version = "rhel"
+        else:
+            version = "Not Found"
+        self.os["Version"] = version
+        return True
 
     def ResetIPTables(self):
         from subprocess import call
@@ -79,26 +91,84 @@ class FireWallInit:
         try:
             # Set NoTouchNets
             for network in self.notouch_nets:
+                call("sudo iptables -A NoTouchHosts -d %s -j LOG" % network)
                 call("sudo iptables -A NoTouchHosts -d %s -j REJECT" % network)
         except Exception as err:
             print("Error setting no touch hosts: %s" % err)
             return False
+
         try:
             # Set KnownBadNets
             for network in self.knownbad_nets:
+                call("sudo iptables -A KnownBadNets -s %s -j LOG" % network)
                 call("sudo iptables -A KnownBadNets -s %s -j DROP" % network)
-                call("sudo iptables -A KnownBadNets -d %s -j DROP" % network)
+                call("sudo iptables -A KnownBadNets -d %s -j LOG" % network)
+                call("sudo iptables -A KnownBadNets -d %s -j REJECT" % network)
         except Exception as err:
             print("Error setting known bad hosts: %s" % err)
             return False
+
         try:
             # Set Peer Hosts
-            for network,exeption in self.peer_nets:
-                for vernet in exception:
+            for network,exceptionnet in self.peer_nets:
+                for net in exceptionnet["out"]:
                     call("sudo iptables -A PeerHosts_Out -d %s -j REJECT" % net)
+                for net in exceptionnet["in"]:
+                    call("sudo iptables -A PeerHosts_In -s %s -j REJECT" % net)
+                call("sudo iptables -A PeerHosts_Out -d %s -j ACCEPT" % network)
+                call("sudo iptables -A PeerHosts_In -s %s -j ACCEPT" % network)
+        except Exception as err:
+            print("Error setting peer hosts: %s" % err)
+            return False
 
-    def KillAuditD:
+        try:
+            # Set SupportSystem Hosts
+            for net in self.support_systems["dns"]:
+                call("sudo iptables -A SupportSystems_In -s %s -p UDP --sport 53 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -p UDP --dport 53 -j ACCEPT" % net)
+            for net in self.support_systems["ntp"]:
+                call("sudo iptables -A SupportSystems_In -s %s -p UDP --sport 123 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -p UDP --dport 123 -j ACCEPT" % net)
+            for net in self.support_systems["share"]:
+                call("sudo iptables -A SupportSystems_In -s %s -P TCP --sport 22 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -P TCP --dport 22 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_In -s %s -p TCP --sport 139 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_In -s %s -p TCP --sport 445 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -p TCP --dport 139 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -p TCP --dport 445 -j ACCEPT" % net)
+            for net in self.support_systems["dhcp"]:
+                call("sudo iptables -A SupportSystems_In -s %s -p UDP --sport 67 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_In -s %s -p UDP --sport 68 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -p UDP --dport 67 -j ACCEPT" % net)
+                call("sudo iptables -A SupportSystems_Out -d %s -p UDP --dport 68 -j ACCEPT" % net)
+        except Exception as err:
+            print("Error setting support system rules: %s" % err)
+            return False
 
+        try:
+            #Set Target Networks
+            for network , exceptionnet in self.target_nets:
+                for net in exceptionnet["in"]:
+                    call("sudo iptables -A TargetNets_In -s %s -j ACCEPT" % net)
+                for net in exceptionnet["out"]:
+                    call("sudo iptables -A TargetNets_Out -d %s -j DENY" % net)
+            call("sudo iptables -A TargetNets_Out -d %s -j ACCEPT" % network)
+            call("sudo iptables -A TargetNets_In -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT")
+        except Exception as err:
+            print("Error setting target network rules: %s" % err)
+            return False
+        return True
+
+    def KillOSFirewall(self):
+        
+        from subprocess import call
+        if self.os["Version"] == "rhel":
+            call("sudo systemctl disable firewalld")
+        elif self.os["Version"] == "ubuntu":
+            call("sudo ufw disable")
+        else:
+            return False
+        return True
 
 class SystemInit:
 
